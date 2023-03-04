43996f0641d1760d0f29e0ebd31b6e7017c12346
ayush18023 <ayushmdcr@gmail.com>
2023-03-04 14:32:46 +0530

    Home page

---
+++ src/Components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+import React from 'react'
+
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+        </div>
+    )
+}
+
+export default Layout
